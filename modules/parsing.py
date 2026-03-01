d88c9b60f3857ec680d931777700ed2797a64b38
Zachary Kenneth Stewart <pro_zac@hotmail.com>
2026-03-01 23:33:45 +1000

    get_chunking_points() func
    This function has been used in a few different places throughout Z.K.S' code base, and most recently as an R version in psQTL's sPLS-DA script. It will be used in subsequent commits to this repo

--- modules/parsing.py
+++ modules/parsing.py
@@ -1,6 +1,6 @@
 #! python3
 
-import gzip, codecs
+import gzip, codecs, math
 from contextlib import contextmanager
 
 def get_codec(fileName):
@@ -99,3 +99,67 @@ def parse_2col_tsv_as_dict(tsvFile, keyIsLeft=True):
             
             tsvDict[left] = right
     return tsvDict
+
+def get_chunking_points(numberToChunk, chunks, isNumOfChunks=True):
+    '''
+    This is a general purpose function to take in a number of "things"
+    that you want to chunk, and find out how to chunk them evenly.
+    
+    The resulting list should be interpreted as the 0-based indices where
+    a new chunk should form. You should check for this index at the start
+    of a loop, and form a new file if your index == the value in this list.
+    
+    Also, this uses "allocated chunking" such that it will try to keep
+    the number of things per chunk approximately equal. Even if you specify
+    X number of things per chunk, it might be more optimal to have X-1 in each
+    chunk so as to make sure the last chunk doesn't contain a single thing.
+    This might not be what you want, but usually, allocated chunking leads to
+    more optimal code (e.g., a major use of this function could be for
+    parallel processing of the chunks).
+    
+    Params:
+        numberToChunk -- an integer value, possibly derived from a list length as example.
+        chunks -- an integer value for the desired number of chunks OR the number of
+                  sequences to contain within each chunk, determined by
+        isNumOfChunks -- a boolean indicating whether you want the number to be the number
+                         of chunks (True), or the number of sequences within each chunk (False)
+    '''
+    assert isinstance(numberToChunk, int)
+    assert isinstance(chunks, int)
+    if numberToChunk < chunks:
+        raise Exception(f"Chunking only valid if chunkSize <= chunks i.e., {chunks} <= {numberToChunk}")
+    
+    # Derive how many chunks we want to split the file into
+    if isNumOfChunks:
+        numChunks = chunks
+    else:
+        numChunks = math.ceil(numberToChunk / chunks)
+    
+    rawNum = numberToChunk / numChunks # This line is more relevant in the multithreading code I took this from, but it's okay to just leave it.
+    numRoundedUp = round((rawNum % 1) * numChunks, 0) # By taking the decimal place and multiplying it by the num of chunks, we can figure out how many chunks need to be rounded up
+    
+    # Store positions at which to start a new chunk
+    chunkPoints = []
+    ongoingCount = 0
+    for i in range(numChunks):
+        
+        # Determine where chunks begin in 0-based indexing
+        if i < numRoundedUp: # decide if the number of sequences in this chunk should be rounded up
+            point = math.ceil(rawNum) + ongoingCount # Round up the rawNum, and also add our ongoingCount which corresponds to the number of things already put into a chunk
+            
+            # Prevent chunking beyond the last index where a chunk should start
+            if point >= numberToChunk: # Without this check, if we have more chunks than things to chunk, we can end up with "extra" numbers in the list (e.g., [1, 2, 3, 4, 5, 6, 6, 6, 6, 6]).
+                break  # This doesn't actually affect program function, but for aesthetic reasons and for clarity of how this function works, I prevent this from occurring.
+            
+            chunkPoints.append(point)
+            ongoingCount += math.ceil(rawNum)
+        else:
+            point = math.floor(rawNum) + ongoingCount # Round down the rawNum since we've already accounted for any extra uneven numbers
+            
+            if point >= numberToChunk:
+                break
+            
+            chunkPoints.append(point)
+            ongoingCount += math.floor(rawNum)
+    
+    return chunkPoints
